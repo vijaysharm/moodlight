8fcd3dd398ab14c655d6f7e324050a6ff9d4f18a
Vijay Sharma <vijay.sharm@gmail.com>
2020-04-20 14:26:57 -0400

    Initial Commit

---
+++ Moodlight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moodlight
+//
+//  Created by Vijay Sharma on 2020-04-20.
+//  Copyright © 2020 Vijay Sharma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
